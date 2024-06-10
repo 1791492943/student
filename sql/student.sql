af51d8aff5088ef659319d79c9467a3a65926df4
czy <1791492943@qq.com>
2024-06-10 23:02:59 +0800

    .

--- sql/student.sql
+++ sql/student.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80029 (8.0.29)
  File Encoding         : 65001
 
- Date: 05/06/2024 20:51:11
+ Date: 10/06/2024 23:02:28
 */
 
 SET NAMES utf8mb4;
@@ -28,7 +28,13 @@ CREATE TABLE `class`  (
   `create_time` datetime NULL DEFAULT NULL COMMENT '创建时间',
   `update_time` datetime NULL DEFAULT NULL COMMENT '更新时间',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '班级表' ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '班级表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of class
+-- ----------------------------
+INSERT INTO `class` VALUES (1, '平面', 18, '2024-06-10 09:41:44', '2024-06-10 09:41:44');
+INSERT INTO `class` VALUES (2, '高复', 18, '2024-06-10 09:41:44', '2024-06-10 09:41:44');
 
 -- ----------------------------
 -- Table structure for punishment
@@ -42,7 +48,17 @@ CREATE TABLE `punishment`  (
   `create_time` datetime NULL DEFAULT NULL COMMENT '创建时间',
   `update_time` datetime NULL DEFAULT NULL COMMENT '更新时间',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '处分表' ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 7 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '处分表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of punishment
+-- ----------------------------
+INSERT INTO `punishment` VALUES (1, '正常', 0, 1, '2024-06-10 09:48:22', '2024-06-10 09:48:22');
+INSERT INTO `punishment` VALUES (2, '通报批评', 1, 2, '2024-06-10 09:48:22', '2024-06-10 09:48:22');
+INSERT INTO `punishment` VALUES (3, '警告', 2, 3, '2024-06-10 09:48:22', '2024-06-10 09:48:22');
+INSERT INTO `punishment` VALUES (4, '严重警告', 3, 4, '2024-06-10 09:48:22', '2024-06-10 09:48:22');
+INSERT INTO `punishment` VALUES (5, '记过', 4, 5, '2024-06-10 09:48:22', '2024-06-10 09:48:22');
+INSERT INTO `punishment` VALUES (6, '留校察看', 5, 6, '2024-06-10 09:48:22', '2024-06-10 09:48:22');
 
 -- ----------------------------
 -- Table structure for student
@@ -52,12 +68,17 @@ CREATE TABLE `student`  (
   `id` bigint NOT NULL AUTO_INCREMENT COMMENT '主键',
   `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '学生姓名',
   `sex` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '性别',
-  `class_id` int NULL DEFAULT NULL COMMENT '班级id',
   `student_code` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '学号',
+  `level` int NULL DEFAULT NULL COMMENT '处分等级',
   `create_time` datetime NULL DEFAULT NULL COMMENT '创建时间',
   `update_time` datetime NULL DEFAULT NULL COMMENT '修改时间',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '学生表' ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '学生表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of student
+-- ----------------------------
+INSERT INTO `student` VALUES (1, '张三', '男', '10001', 2, '2024-06-08 20:00:05', '2024-06-08 20:00:05');
 
 -- ----------------------------
 -- Table structure for student_class
@@ -70,7 +91,13 @@ CREATE TABLE `student_class`  (
   `create_time` datetime NULL DEFAULT NULL COMMENT '创建时间',
   `update_time` datetime NULL DEFAULT NULL COMMENT '更新时间',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '学生班级关联表' ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '学生班级关联表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of student_class
+-- ----------------------------
+INSERT INTO `student_class` VALUES (1, 1, 1, '2024-06-10 09:42:25', '2024-06-10 09:42:25');
+INSERT INTO `student_class` VALUES (2, 2, 1, '2024-06-10 09:42:25', '2024-06-10 09:42:25');
 
 -- ----------------------------
 -- Table structure for student_punishment
@@ -80,14 +107,25 @@ CREATE TABLE `student_punishment`  (
   `id` bigint NOT NULL AUTO_INCREMENT COMMENT '主键',
   `student_id` bigint NULL DEFAULT NULL COMMENT '学生id',
   `punishment_level` bigint NULL DEFAULT NULL COMMENT '处分等级',
+  `criticism` int NULL DEFAULT 0 COMMENT '1:表示通报批评',
   `remark` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '备注',
   `year` int NULL DEFAULT NULL COMMENT '当前年份',
   `semester` int NULL DEFAULT NULL COMMENT '学期',
   `punishment_time` datetime NULL DEFAULT NULL COMMENT '处分时间',
+  `change` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '变化(up down noChange)',
   `create_time` datetime NULL DEFAULT NULL COMMENT '创建时间',
   `update_time` datetime NULL DEFAULT NULL COMMENT '更新时间',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '学生处分关联表' ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 6 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '学生处分关联表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of student_punishment
+-- ----------------------------
+INSERT INTO `student_punishment` VALUES (1, 1, 2, 0, '上课玩手机', 18, 1, '2024-06-09 09:47:09', 'up', '2024-06-09 09:47:09', '2024-06-09 09:47:09');
+INSERT INTO `student_punishment` VALUES (2, 1, 3, 0, '上课玩手机', 18, 1, '2024-06-10 09:47:09', 'up', '2024-06-10 09:47:09', '2024-06-10 09:47:09');
+INSERT INTO `student_punishment` VALUES (3, 1, 2, 0, '表现良好', 18, 1, '2024-06-10 11:33:09', 'down', '2024-06-10 11:33:09', '2024-06-10 11:33:09');
+INSERT INTO `student_punishment` VALUES (4, 1, 2, 1, '跑步不达标', 18, 1, '2024-06-10 11:39:23', 'noChange', '2024-06-10 11:39:23', '2024-06-10 11:39:23');
+INSERT INTO `student_punishment` VALUES (5, 1, 3, 1, '跑步不达标', 18, 1, '2024-06-10 11:51:07', 'up', '2024-06-10 11:51:07', '2024-06-10 11:51:07');
 
 -- ----------------------------
 -- Table structure for user
@@ -103,5 +141,9 @@ CREATE TABLE `user`  (
   `update_time` datetime NULL DEFAULT NULL COMMENT '更新时间',
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '用户表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of user
+-- ----------------------------
 
 SET FOREIGN_KEY_CHECKS = 1;
